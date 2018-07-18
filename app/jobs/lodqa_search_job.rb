ce248f71b373f8217d6feb895498b23caa97ecf6
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-07-18 19:29:00 +0900

    Save answers to database

--- app/jobs/lodqa_search_job.rb
+++ app/jobs/lodqa_search_job.rb
@@ -9,20 +9,21 @@ class LodqaSearchJob < ApplicationJob
   end
 
   def perform(*args)
+    request_id = job_id
     query = args[0]
 
-    result = execute query do
+    result = execute request_id, query do
       # TODO: call the callback with an accepting query event
-      p 'Searching the query have been starting.'
+      p "Searching the query #{request_id} have been starting."
     end
 
     # TODO: call the callback with finishing query event
     p result
   end
 
-  def execute(query)
+  def execute(request_id, query)
     start_time = Time.now
-    threads = execute_on_all_datasets query
+    threads = execute_on_all_datasets request_id, query
 
     yield
 
@@ -31,14 +32,26 @@ class LodqaSearchJob < ApplicationJob
       JSON.pretty_generate(answer_set.map { |k, v| { url: k, label: v } })
   end
 
-  def execute_on_all_datasets(query)
+  def execute_on_all_datasets(request_id, query)
     Lodqa::Sources.datasets.map do |dataset|
       Thread.start do
-        executor = Lodqa::OneByOneExecutor.new dataset, query
+        executor = Lodqa::OneByOneExecutor.new dataset, query, debug: false
 
         # Bind events to colletc answers
         collected_answers = []
-        executor.on(:answer) { |_, val| collected_answers << val[:answer] }
+        executor.on(:answer) do |_, val|
+          begin
+            Answer.create request_id: request_id, uri: val[:answer][:uri], label: val[:answer][:label]
+
+            collected_answers << val[:answer]
+          rescue ActiveRecord::RecordNotUnique
+            Rails.logger.debug "Duplicated answer: request_id: #{request_id}, uri: #{val[:answer][:uri].to_s}"
+          rescue => e
+            Rails.logger.orrer "#{e.class}, #{e.message}"
+          ensure
+            ActiveRecord::Base.connection_pool.checkin Answer.connection
+          end
+        end
 
         executor.perform
 
