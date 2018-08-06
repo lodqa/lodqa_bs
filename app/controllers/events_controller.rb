e4419efbe39514566ab5f6d23e09f6ab67ce01b2
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-08-06 16:13:10 +0900

    Add a route to event

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# To get old events of the query.
+class EventsController < ApplicationController
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :queries, only: :create 
+  resources :queries, only: :create do
+    resources :events, only: :index
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
