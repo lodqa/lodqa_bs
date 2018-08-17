e1c2fd8724140368577b9dd3a856178429ad6754
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-08-17 12:31:28 +0900

    Remove unused EventController

--- app/controllers/events_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# To get old events of the query.
-class EventsController < ApplicationController
-  def index
-    render json: Event.where(query_id: params[:query_id]).pluck(:data)
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :queries, only: :create do
-    resources :events, only: :index
     resources :subscriptions, only: :create
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
