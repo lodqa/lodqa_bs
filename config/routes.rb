012f04531591ae2a2c64a4519848d62475681d8f
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-08-20 16:32:06 +0900

    Verify the query_id in the URL for subscription

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :queries, only: :create do
+  resources :queries,
+            only: :create,
+            constraints: {id: /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/} do
     resources :subscriptions, only: :create
   end
 end
