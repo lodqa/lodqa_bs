b3a3df4382bf6ff49bde7f3000b7e95fa33c103c
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-08-20 16:46:49 +0900

    Verify the callback_url parameter of the Subscription controller

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -9,6 +9,18 @@ class SubscriptionsController < ApplicationController
                     status: :not_found
     end
 
+    return render nothing: true, status: :bad_request unless valid_url? params[:callback_url]
+
     NotificationJob.perform_now query_id, params[:callback_url]
+  end
+
+  private
+
+  # see https://stackoverflow.com/a/9047226/1276969
+  def valid_url? value
+    uri = URI.parse value
+    uri.is_a? URI::HTTP
+  rescue URI::InvalidURIError
+    false
   end
 end
