8d25d789d9e76004f8d60919ba645ee7ce8840b7
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-08-29 14:51:59 +0900

    Remove unnecessary error handling

--- lib/lodqa/source_error.rb
+++ lib/lodqa/source_error.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Lodqa
-  class SourceError < StandardError; end
+  class SourceError < IOError; end
 end
--- lib/lodqa/sources.rb
+++ lib/lodqa/sources.rb
@@ -9,17 +9,13 @@ module Lodqa
 
     class << self
       def datasets target_url = DEFAULT_URL
-        RestClient.get target_url do |response, _request, _result|
+        RestClient.get target_url do |response|
           case response.code
           when 200 then JSON.parse response, symbolize_names: true
           else
-            Logger.error nil, message: "Configuration Server retrun an error for #{target_url}", response_code: response.code, response_body: response.body
-            raise IOError, "Response Error for url: #{target_url}"
+            raise SourceError, "Configuration Server retrun an error for #{target_url}. response_code: #{response.code}, response_body: #{response.body}"
           end
         end
-      rescue StandardError => e
-        Logger.error e, message: "Cannot connect the Configuration Server for #{target_url}"
-        raise SourceError, "invalid url #{target_url}"
       end
     end
   end
