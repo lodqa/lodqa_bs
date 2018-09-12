fdfc8028a319da292d1f48d58d93451123bd6772
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-09-12 16:33:05 +0900

    refactor: Move the channel into labor

--- app/labor/channel.rb
+++ app/labor/channel.rb
@@ -4,21 +4,25 @@
 # All channesls share unreachable url.
 class Channel
   # A Set of urls that is failed to send any message.
-  @@unreachable_url = Set.new # rubocop:disable Style/ClassVars
+  @unreachable_url = Set.new
+
+  class << self
+    attr_reader :unreachable_url
+  end
 
   def initialize url
     @url = url
 
     # Delete re-registered url.
-    @@unreachable_url.delete url
+    self.class.unreachable_url.delete url
   end
 
   # Transmit a event of the search to subscribers.
   def transmit data
-    return if @unreachable_url.member? @url
+    return if self.class.unreachable_url.member? @url
     HTTP.post @url, data
-  rescue Errno::ECONNREFUSED, Net::OpenTimeout, SocketError => e
-    logger.info "Establishing TCP connection to #{url} failed. Error: #{e.inspect}"
-    @@unreachable_url << url
+  rescue Errno::ECONNREFUSED, Net::OpenTimeout, SocketError
+    self.class.unreachable_url << url
+    raise
   end
 end
