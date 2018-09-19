c32a5118169c4a8b03d68c1df23ed52f372a34d8
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-09-19 13:49:26 +0900

    refactor: Rename variables and fix comments

--- app/services/register_search_service.rb
+++ app/services/register_search_service.rb
@@ -3,34 +3,35 @@
 # Bussiness logic about registering a serach
 module ReigsterSearchService
   class << self
-    # Register a query.
-    # return search_id if same query exists.
+    # Register a search.
+    # Start a new search job unless same search exists.
+    # Call back only if same search exists.
     def register search
-      dupulicate_query = Search.equals_in search
+      dupulicate_search = Search.equals_in search
 
-      return send_callback_about dupulicate_query if dupulicate_query
+      return start_new_job_for search unless dupulicate_search
 
-      start_new_job_for search
+      call_back_events_about dupulicate_search
     end
 
     private
 
     # Call back events about an exiting search.
-    def send_callback_about query
-      case query.state
+    def call_back_events_about search
+      case search.state
+      # when :aborted Aborted seraches do not match new queries.
       # when :queued Callbacks will be called after the job start.
       when :running
-        EventSender.send_to query.start_search_callback_url,
-                            query.data_for_start_event
-      # when :aborted Aborted seraches do not match new queries.
+        EventSender.send_to search.start_search_callback_url,
+                            search.data_for_start_event
       when :finished
-        EventSender.send_to query.start_search_callback_url,
-                            query.data_for_start_event
-        EventSender.send_to query.finish_search_callback_url,
-                            query.dafa_for_finish_event
+        EventSender.send_to search.start_search_callback_url,
+                            search.data_for_start_event
+        EventSender.send_to search.finish_search_callback_url,
+                            search.dafa_for_finish_event
       end
 
-      query.search_id
+      search.search_id
     end
 
     # Start new job for new search.
