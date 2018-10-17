96e6f5d0811118b8463b228810aefdc6ccb65b9c
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-10-17 15:12:08 +0900

    HTTPConnection suspends the remaining data transmission if the received response is a not found or a server error

--- app/lib/http_connection.rb
+++ app/lib/http_connection.rb
@@ -5,9 +5,22 @@ module HTTPConnection
   def self.send url, *data
     HTTP.start(url) do |http_conn|
       data.each do |datum|
-        error = http_conn.post datum
-        next unless error
-        Rails.logger.error "Request to callback url is failed. URL: #{url}, error_message: #{error.message}"
+        res = http_conn.post datum
+
+        next unless res
+
+        Rails.logger.error "Request to url is failed. URL: #{url}, error_message: #{res.message}"
+        case res
+        when Net::HTTPNotFound
+          # The URL is static.
+          break
+        when Net::HTTPClientError
+          # Try to next data.
+          next
+        when Net::HTTPServerError
+          # Suspends the remaining data transmission if the received response is a server error.
+          break
+        end
       end
     end
   end
