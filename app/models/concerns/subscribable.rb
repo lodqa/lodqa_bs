f9664b93352260edb2d8cdc0ecaf64ae4d3a5bcc
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-10-17 13:59:36 +0900

    refactor: Extract a method to divide transmission data

--- app/models/concerns/subscribable.rb
+++ app/models/concerns/subscribable.rb
@@ -21,13 +21,16 @@ module Subscribable
   def notify_existing_events_to url, search
     events = DbConnection.using { Event.occurred_for search }
     # pp events.count
-    while events.any?
-      parts = events.shift 100
+    split(events).each do |parts|
       error = HTTP.post url,
                         events: parts
       if error
         break logger.error "Request to callback url is failed. URL: #{url}, error_message: #{error}"
       end
     end
+  end
+
+  def split events
+    events.each_slice(100).to_a
   end
 end
