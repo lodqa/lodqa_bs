10793a2a96355b7695394ccd3dd726060038d578
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-10-22 14:46:58 +0900

    bugifx: When transmission data is sufficiently small, transmission data is not split. At this time the transmission data was broken.

--- app/models/concerns/subscribable.rb
+++ app/models/concerns/subscribable.rb
@@ -30,7 +30,7 @@ module Subscribable
     return events unless events.any?
 
     total_size = events.to_json.length
-    return events if total_size <= TRANSMIT_DATA_SIZE_UPPER_LIMIT
+    return [events] if total_size <= TRANSMIT_DATA_SIZE_UPPER_LIMIT
 
     # One transmission data size is calculated by the number of events.
     # Please note that if an event is huge, the send data size limit may be exceeded.
