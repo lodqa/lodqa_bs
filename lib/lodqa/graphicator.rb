4b3bf9a3f05e0c926787215bf7c9fa585fb8b604
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2019-02-20 18:28:26 +0900

    refactor: Refactor the Graphicator

--- lib/lodqa/graphicator.rb
+++ lib/lodqa/graphicator.rb
@@ -8,111 +8,116 @@ require 'pp'
 
 module Lodqa; end unless defined? Lodqa
 
-# An instance of this class is initialized with a dictionary.
-class Lodqa::Graphicator
-  attr_reader :parser
+module Lodqa::Graphicator
+  class << self
+    def produce_pseudo_graph_pattern query, parser_url
+      raise ArgumentError, 'query should be given.' if query.nil? || query.empty?
+      raise ArgumentError, 'parser_url should be given.' if parser_url.nil? || parser_url.empty?
 
-  def initialize parser_url
-    raise ArgumentError, 'parser_url should be given.' if parser_url.nil? || parser_url.empty?
-
-    @parser = EnjuAccess::CGIAccessor.new(parser_url)
-  end
-
-  def parse query
-    @parse = @parser.parse(query)
-    self
-  end
-
-  def pgp
-    graphicate(@parse)
-  end
-
-  def graphicate parse
-    # [Exception Handling] Treat the entire sentence as a BNC when no BNC was found
-    if parse[:base_noun_chunks].empty?
-      last_idx = parse[:tokens].last[:idx]
-      parse[:base_noun_chunks] << { head: last_idx, beg: 0, end: last_idx }
+      produce_pgp_from query, parser_url
     end
 
-    nodes = get_nodes(parse)
+    private
 
-    # index the nodes by their heads
-    node_index = {}
-    nodes.each_key { |k| node_index[nodes[k][:head]] = k }
-
-    # get the focus
-    focus = node_index[parse[:focus]]
-
-    # default rule: take the first one as the focus, if no grammatical focus is found.
-    focus = node_index.values.first if focus.nil?
-
-    edges = get_edges(parse, node_index)
-
-    post_processing!(nodes, edges)
-
-    {
-      nodes: nodes,
-      edges: edges,
-      focus: focus.to_s
-    }
-  end
-
-  def get_nodes parse
-    nodes = {}
-
-    variable = 't0'
-    parse[:base_noun_chunks].each do |c|
-      variable = variable.next
-      nodes[variable.to_sym] = {
-        head: c[:head],
-        text: parse[:tokens][c[:beg]..c[:end]].collect { |t| t[:lex] }.join(' ')
-      }
+    def produce_pgp_from query, parser_url
+      parsed_query = parse parser_url, query
+      graphicate parsed_query
     end
 
-    nodes
-  end
-
-  def get_edges parse, node_index
-    parse[:relations].collect do |s, p, o|
-      {
-        subject: node_index[s].to_s,
-        object: node_index[o].to_s,
-        text: p.collect { |i| parse[:tokens][i][:lex] }.join(' ')
-      }
+    def parse parser_url, query
+      EnjuAccess::CGIAccessor.new(parser_url).parse(query)
     end
-  end
 
-  # post_processing may be dependent on Enju
-  def post_processing! nodes, edges
-    # 'and' coordination
-    edges.reject! { |e| e[:text] == 'and' }
-
-    # 'have A as B' pattern
-    edges_have_as = edges.find_all { |e| e[:text] == 'have as' }
-    unless edges_have_as.empty?
-      pairs_have_as = edges_have_as.group_by { |e| e[:object] }
-      pairs_have_as.each do |obj, pair|
-        edge_have = edges.find do |e|
-          e[:text] == 'have' &&
-            ((e[:subject] == pair.first[:subject] && e[:object] == pair.last[:subject]) || (e[:subject] == pair.last[:subject] && e[:object] == pair.first[:subject]))
-        end
-        if edge_have
-          edge_have[:text] = nodes[obj.to_sym][:text]
-          nodes.delete(obj.to_sym)
-        end
+    def graphicate parsed_query
+      # [Exception Handling] Treat the entire sentence as a BNC when no BNC was found
+      if parsed_query[:base_noun_chunks].empty?
+        last_idx = parsed_query[:tokens].last[:idx]
+        parsed_query[:base_noun_chunks] << { head: last_idx, beg: 0, end: last_idx }
       end
-      edges.reject! { |e| e[:text] == 'have as' }
+
+      nodes = nodes_from parsed_query
+
+      # index the nodes by their heads
+      node_index = node_index_from nodes
+
+      edges = edges_from parsed_query, node_index
+
+      post_process! nodes, edges
+
+      focus = focus_from node_index, parsed_query
+
+      {
+        nodes: nodes,
+        edges: edges,
+        focus: focus.to_s
+      }
     end
 
-    # 'have A and B as C' pattern
-    edges_have_and_as = edges.find_all { |e| e[:text] == 'have and as' }
-    edges_have_and_as.each do |edge_have_and_as|
-      edges_and_as = edges.find_all { |e| e[:text] == 'and as' && e[:object] == edge_have_and_as[:object] }
-      edges_have_and = edges_and_as.map { |edge_and_as| edges.find { |e| e[:text] == 'have and' && e[:object] == edge_and_as[:subject] } }
-      edges_have_and.each { |e| e[:text] = nodes[edge_have_and_as[:object].to_sym][:text] }
-      nodes.delete(edge_have_and_as[:object].to_sym)
+    def nodes_from parsed_query
+      variable = 't0'
+      parsed_query[:base_noun_chunks].each_with_object({}) do |c, nodes|
+        variable = variable.next
+        nodes[variable.to_sym] = {
+          head: c[:head],
+          text: parsed_query[:tokens][c[:beg]..c[:end]].collect { |t| t[:lex] }.join(' ')
+        }
+      end
     end
-    edges.reject! { |e| e[:text] == 'have and as' }
-    edges.reject! { |e| e[:text] == 'and as' }
+
+    def node_index_from nodes
+      node_index = {}
+      nodes.each_key { |k| node_index[nodes[k][:head]] = k }
+
+      node_index
+    end
+
+    def edges_from parsed_query, node_index
+      parsed_query[:relations].collect do |s, p, o|
+        {
+          subject: node_index[s].to_s,
+          object: node_index[o].to_s,
+          text: p.collect { |i| parsed_query[:tokens][i][:lex] }.join(' ')
+        }
+      end
+    end
+
+    def focus_from node_index, parsed_query
+      # default rule: take the first one as the focus, if no grammatical focus is found.
+      node_index[parsed_query[:focus]] || node_index.values.first
+    end
+
+    # post_processing may be dependent on Enju
+    def post_process! nodes, edges
+      # 'and' coordination
+      edges.reject! { |e| e[:text] == 'and' }
+
+      # 'have A as B' pattern
+      edges_have_as = edges.find_all { |e| e[:text] == 'have as' }
+      unless edges_have_as.empty?
+        pairs_have_as = edges_have_as.group_by { |e| e[:object] }
+        pairs_have_as.each do |obj, pair|
+          edge_have = edges.find do |e|
+            e[:text] == 'have' &&
+              ((e[:subject] == pair.first[:subject] && e[:object] == pair.last[:subject]) || (e[:subject] == pair.last[:subject] && e[:object] == pair.first[:subject]))
+          end
+          if edge_have
+            edge_have[:text] = nodes[obj.to_sym][:text]
+            nodes.delete(obj.to_sym)
+          end
+        end
+        edges.reject! { |e| e[:text] == 'have as' }
+      end
+
+      # 'have A and B as C' pattern
+      edges_have_and_as = edges.find_all { |e| e[:text] == 'have and as' }
+      edges_have_and_as.each do |edge_have_and_as|
+        edges_and_as = edges.find_all { |e| e[:text] == 'and as' && e[:object] == edge_have_and_as[:object] }
+        edges_have_and = edges_and_as.map { |edge_and_as| edges.find { |e| e[:text] == 'have and' && e[:object] == edge_and_as[:subject] } }
+        edges_have_and.each { |e| e[:text] = nodes[edge_have_and_as[:object].to_sym][:text] }
+        nodes.delete(edge_have_and_as[:object].to_sym)
+      end
+      edges.reject! { |e| e[:text] == 'have and as' }
+      edges.reject! { |e| e[:text] == 'and as' }
+    end
   end
 end
--- lib/lodqa/pgp_factory.rb
+++ lib/lodqa/pgp_factory.rb
@@ -5,7 +5,7 @@ require 'lodqa/graphicator'
 module Lodqa
   module PGPFactory
     def self.create parser_url, query
-      Graphicator.new(parser_url).parse(query).pgp
+      Graphicator.produce_pseudo_graph_pattern query, parser_url
     end
   end
 end
