fcac7326beb3d027fedb6f5c22b0d6274f07894e
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-01-14 13:27:58 +0900

    chore: Disable ActionDispatch::HostAuthorization

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,4 +47,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Disable ActionDispatch::HostAuthorization
+  config.hosts.clear
 end
