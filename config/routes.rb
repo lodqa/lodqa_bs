0bfda9089b4aad3dec48df141a9d84432b7a7f0f
sharsukhchuluun <sukhchuluun@luxiar.com>
2020-03-10 13:26:23 +0900

    issue #27 routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
   resources :searches,
-            only: [:index, :show, :destroy],
+            only: [:show], as: :searches_api,
+            controller: :searches_api,
+            constraints: {id: /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/, format: :json }
+  resources :searches,
+            only: [:index, :destroy],
             controller: :searches_view
   resources :searches,
-            only: [:show, :create],
+            only: [:create],
             controller: :searches_api,
             constraints: {id: /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/} do
     resources :subscriptions, only: :create
   end
+  get 'searches/:id', to: 'searches_view#show', as: :search_show
   resources :dialogs,
             only: [:index],
             controller: :dialogs
