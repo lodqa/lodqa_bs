39276eef650a98139a4f03b6d9ff6b75edad4449
sharsukhchuluun <sukhchuluun@luxiar.com>
2020-03-11 11:53:39 +0900

    issue #27 定数不要

--- app/controllers/user_histories_controller.rb
+++ app/controllers/user_histories_controller.rb
@@ -2,8 +2,6 @@
 
 # A controller to show registered dialogs.
 class UserHistoriesController < ActionController::Base
-  ITEMS_PER_PAGE = 10
-
   # Get dialogs for user_id
   def show
     dialogs = Dialog.user_dialogs(params[:id])
