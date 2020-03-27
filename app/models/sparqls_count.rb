77ceac0b7247bc21268e653cb5d7652a7ff52507
sharsukhchuluun <sukhchuluun@luxiar.com>
2020-03-27 13:30:19 +0900

    issue #35 include不要

--- app/models/sparqls_count.rb
+++ app/models/sparqls_count.rb
@@ -8,8 +8,6 @@ require 'sparql_client/cacheable_client'
 # returns the count of sparqls
 class SparqlsCount
   class << self
-    include Logger::Loggable
-
     # Handle sparqls count.
     def sparqls_count param
       parallel = 16
@@ -20,7 +18,7 @@ class SparqlsCount
 
       sparqls_count = 0
       anchored_pgps = Lodqa::AnchoredPgps.new param.pgp, param.mappings
-      anchored_pgps.logger = Logger::Logger.new 'sparqls_count', logger, Logger::INFO
+      anchored_pgps.logger = Logger::Logger.new 'sparqls_count', Rails.logger, Logger::INFO
       anchored_pgps.each do |anchored_pgp|
         graph_finder.sparqls_of(anchored_pgp) do |_bgp, _sparql|
           sparqls_count += 1
