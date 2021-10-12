626b2b92b4917140e193f3e39e0f829843182363
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-10-12 13:08:26 +0900

    feature: Allow host name for docker environment of the LODQA

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Allow host name for docker environment of LODQA.
+  config.hosts << 'lodqa_bs'
 end
