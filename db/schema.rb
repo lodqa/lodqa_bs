83a72ed82bba4927c92311bf03b9ff711dbe8e4c
ledsun <shigeru.nakajima@gmail.com>
2023-05-30 16:25:14 +0900

    misc: Update schema file for Rails 7.0

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[6.1].define(version: 2020_02_07_064024) do
-
+ActiveRecord::Schema[7.0].define(version: 2020_02_07_064024) do
   create_table "dialogs", force: :cascade do |t|
     t.string "user_id", null: false
     t.integer "search_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["search_id"], name: "index_dialogs_on_search_id"
   end
 
   create_table "events", force: :cascade do |t|
     t.string "event", null: false
     t.text "data", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "pseudo_graph_pattern_id"
     t.index ["event"], name: "index_events_on_event"
     t.index ["pseudo_graph_pattern_id"], name: "index_events_on_pseudo_graph_pattern_id"
@@ -37,22 +36,22 @@ ActiveRecord::Schema[6.1].define(version: 2020_02_07_064024) do
     t.integer "answer_limit", default: 10, null: false
     t.string "target", default: "", null: false
     t.boolean "private", default: false, null: false
-    t.datetime "started_at"
-    t.datetime "finished_at"
-    t.datetime "aborted_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "started_at", precision: nil
+    t.datetime "finished_at", precision: nil
+    t.datetime "aborted_at", precision: nil
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "searches", force: :cascade do |t|
     t.string "search_id", limit: 36, null: false
     t.string "query", default: "", null: false
-    t.datetime "created_at", default: "-4712-01-01 00:00:00", null: false
-    t.datetime "started_at"
-    t.datetime "finished_at"
-    t.datetime "aborted_at"
+    t.datetime "created_at", precision: nil, default: "-4712-01-01 00:00:00", null: false
+    t.datetime "started_at", precision: nil
+    t.datetime "finished_at", precision: nil
+    t.datetime "aborted_at", precision: nil
     t.integer "pseudo_graph_pattern_id"
-    t.datetime "referred_at"
+    t.datetime "referred_at", precision: nil
     t.index ["pseudo_graph_pattern_id"], name: "index_searches_on_pseudo_graph_pattern_id"
     t.index ["search_id"], name: "index_searches_on_search_id", unique: true
   end
@@ -61,8 +60,8 @@ ActiveRecord::Schema[6.1].define(version: 2020_02_07_064024) do
     t.integer "pseudo_graph_pattern_id", null: false
     t.string "dataset_name", null: false
     t.text "mapping", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["pseudo_graph_pattern_id"], name: "index_term_mappings_on_pseudo_graph_pattern_id"
   end
 
