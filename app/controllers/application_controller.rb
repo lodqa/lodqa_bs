9cc06d013565091b70c5c76c003e0ed127562744
ledsun <shigeru.nakajima@gmail.com>
2023-06-06 18:08:18 +0900

    fix: Controllor definiton for views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = I18n.default_locale
+  end
 end
