b9cbec05b73e8c05bf6bc01ae359119eed817fa8
ledsun <shigeru.nakajima@gmail.com>
2023-06-13 16:14:08 +0900

    refactor: Fix Rubocop warnings

--- app/models/contextualized_natural_language_expression.rb
+++ app/models/contextualized_natural_language_expression.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Contextualized natural language expression.
 class ContextualizedNaturalLanguageExpression < ApplicationRecord
   belongs_to :dialog
   has_one :search, dependent: :destroy
--- app/models/natural_language_expression.rb
+++ app/models/natural_language_expression.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Natural language expression.
 class NaturalLanguageExpression < ApplicationRecord
   belongs_to :dialog
 end
