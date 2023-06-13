c098931b5e0f5f851677185b8064b0bde2dc534b
ledsun <shigeru.nakajima@gmail.com>
2023-06-13 16:43:49 +0900

    refactor: Add ContextualizeService

--- app/models/dialog.rb
+++ app/models/dialog.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
+# Dialog.
+# A dialog is a sequence of natural language expressions.
 class Dialog < ApplicationRecord
+  has_many :natural_language_expressions, dependent: :destroy
+  has_many :contextualized_natural_language_expressions, dependent: :destroy
+
+  def context = natural_language_expressions
 end
