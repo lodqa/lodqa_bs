6633e06ec28530e80d5102d1a83da1b7f65057d4
ledsun <shigeru.nakajima@gmail.com>
2023-06-14 17:22:51 +0900

    feature: Add setting for contextualizer

--- config/initializers/contextualizer.rb
+++ config/initializers/contextualizer.rb
@@ -1,3 +1,10 @@
 OpenAI.configure do |config|
   config.access_token = ENV.fetch("OPENAI_API_KEY")
 end
+
+Rails.application.configure do
+  config.contextulizer = {
+    instruction: "Make the following sentences into one sentence:",
+    dialog_depth: 2,
+  }
+end
