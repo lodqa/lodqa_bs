f6929b4b69747a3e27ceb6b935cc6ef1348a201b
ledsun <shigeru.nakajima@gmail.com>
2023-06-15 11:35:22 +0900

    refactor: Refactor spec

--- spec/models/contextualizer_spec.rb
+++ spec/models/contextualizer_spec.rb
@@ -4,34 +4,34 @@ require 'rails_helper'
 
 RSpec.describe Contextualizer do
   let(:dialog) { Dialog.create(user_id: 1) }
+  let(:contextualizer) do
+    dialog.natural_language_expressions.create(query: 'Hello.')
+    described_class.new dialog
+  end
 
   describe 'contextualize' do
     it 'returns the contextualized natural language expression' do
-      dialog.natural_language_expressions.create(query: 'Hello')
-
-      contextualizer = described_class.new dialog
       cnle = contextualizer.contextualize
 
       expect(dialog.contextualized_natural_language_expressions.count).to eq 1
-      expect(cnle.query).to eq 'Hello'
+      expect(cnle.query).to eq 'Hello.'
     end
   end
 
   describe 'prompt' do
     it 'returns instruction and context' do
-      dialog.natural_language_expressions.create(query: 'Hello.')
-
-      contextualizer = described_class.new dialog
       prompt = contextualizer.prompt
 
       expect(prompt).to eq 'Make the following sentences into one sentence: Hello.'
     end
 
     context 'when query has no period or question mark' do
-      it 'returns sentence with period' do
+      let(:contextualizer) do
         dialog.natural_language_expressions.create(query: 'Hello')
+        described_class.new dialog
+      end
 
-        contextualizer = described_class.new dialog
+      it 'returns sentence with period' do
         prompt = contextualizer.prompt
 
         expect(prompt).to eq 'Make the following sentences into one sentence: Hello.'
@@ -39,13 +39,30 @@ RSpec.describe Contextualizer do
     end
 
     context 'when query has question mark' do
-      it 'returns sentence with question mark' do
+      let(:contextualizer) do
         dialog.natural_language_expressions.create(query: 'Hello?')
+        described_class.new dialog
+      end
 
-        contextualizer = described_class.new dialog
+      it 'returns sentence with question mark' do
         prompt = contextualizer.prompt
 
         expect(prompt).to eq 'Make the following sentences into one sentence: Hello?'
+      end
+    end
+
+    context 'when dialog is longer than dialog depth' do
+      let(:contextualizer) do
+        dialog.natural_language_expressions.create(query: 'Hello.')
+        dialog.natural_language_expressions.create(query: 'How are you?')
+        dialog.natural_language_expressions.create(query: 'I am fine.')
+        described_class.new dialog
+      end
+
+      it 'returns only the last dialog depth sentences' do
+        prompt = contextualizer.prompt
+
+        expect(prompt).to eq 'Make the following sentences into one sentence: How are you? I am fine.'
       end
     end
   end
