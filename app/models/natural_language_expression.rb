4d426fd3979c2f236664da15cf9a1d46631a6bf7
ledsun <shigeru.nakajima@gmail.com>
2023-06-16 13:06:42 +0900

    refactor: Extract a constant

--- app/models/natural_language_expression.rb
+++ app/models/natural_language_expression.rb
@@ -2,11 +2,13 @@
 
 # Natural language expression.
 class NaturalLanguageExpression < ApplicationRecord
+  STOP_SENTENCE = 'Begin new search'
+
   belongs_to :dialog
 
-  scope :stop_sentence, -> { where query: 'Begin new search' }
+  scope :stop_sentence, -> { where query: STOP_SENTENCE }
 
   def stop_sentence?
-    query == 'Begin new search'
+    query == STOP_SENTENCE
   end
 end
