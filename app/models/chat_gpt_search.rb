f7cf874594552baa5657687874713da237161102
ledsun <shigeru.nakajima@gmail.com>
2023-10-05 14:45:55 +0900

    Fix style by rubocop auto correct

--- app/models/chat_gpt_search.rb
+++ app/models/chat_gpt_search.rb
@@ -16,7 +16,7 @@ class ChatGptSearch
   # Do not do contextualization.
   def run
     pgp = Lodqa::Graphicator.produce_pseudo_graph_pattern @query
-    pseudo_graph_pattern = PseudoGraphPattern.create pgp:, query: @query, target: target
+    pseudo_graph_pattern = PseudoGraphPattern.create(pgp:, query: @query, target:)
     search = create_search pseudo_graph_pattern
     SearchJob.perform_later search.search_id
     search.search_id
