33126d4c21461e1a11c682b0676698d64c43fe2c
ledsun <shigeru.nakajima@gmail.com>
2023-10-05 14:38:58 +0900

    The API for the ChatGPT Plugin automatically sets the target.

--- app/models/chat_gpt_search.rb
+++ app/models/chat_gpt_search.rb
@@ -15,7 +15,7 @@ class ChatGptSearch
   # Do not do contextualization.
   def run
     pgp = Lodqa::Graphicator.produce_pseudo_graph_pattern @query
-    pseudo_graph_pattern = PseudoGraphPattern.create pgp:, query: @query
+    pseudo_graph_pattern = PseudoGraphPattern.create pgp:, query: @query, target: Lodqa::Sources.targets.join(', ')
     search = create_search pseudo_graph_pattern
     SearchJob.perform_later search.search_id
     search.search_id
