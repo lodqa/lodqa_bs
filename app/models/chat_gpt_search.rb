3ef5b29ec7c606ab7aacf867d81f0ee191673e2d
ledsun <shigeru.nakajima@gmail.com>
2023-10-05 15:01:48 +0900

    Look for duplicate searches in the API for the ChatGPT plugin.

--- app/models/chat_gpt_search.rb
+++ app/models/chat_gpt_search.rb
@@ -12,10 +12,17 @@ class ChatGptSearch
   # This is simple prototype search.
   # Only do start search job.
   # Do not do callback.
-  # Do not do duplicate check.
   # Do not do contextualization.
   def run
     pgp = Lodqa::Graphicator.produce_pseudo_graph_pattern @query
+
+    duplicated_pgp = PseudoGraphPattern.equals_in pgp,
+                                                  5,
+                                                  100,
+                                                  10,
+                                                  target
+    return duplicated_pgp.search.search_id if duplicated_pgp
+
     pseudo_graph_pattern = PseudoGraphPattern.create(pgp:, query: @query, target:)
     search = create_search pseudo_graph_pattern
     SearchJob.perform_later search.search_id
